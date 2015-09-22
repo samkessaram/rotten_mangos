a66f930d20f8c2e3af45d538bd7f9cebc0ceea06
samkessaram <samkessaram@gmail.com>
2015-09-22 16:52:38 +0000

    flash bombs

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,12 +14,38 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_path, notice: "#{@movie.title} was submitted successfully!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    if @movie.update_attributes(movie_params)
+      redirect_to movies_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+  end
+
+  protected
+
+  def movie_params
+    params.require(:movie).permit(
+      :title, :relase_Date, :director, :runtime_in_minutes, :poster_image_ur, :description
+    )
+  end
+
 end
-
-  # get 'movies/index'
-
-  # get 'movies/show'
-
-  # get 'movies/new'
-
-  # get 'movies/edit'
