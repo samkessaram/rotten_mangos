c6672593fc006531763d157a93968176a15d27d5
samkessaram <samkessaram@gmail.com>
2015-09-23 02:10:44 +0000

    screwy images

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,9 +6,11 @@ class Movie < ActiveRecord::Base
   validates :director, presence: true
   validates :runtime_in_minutes, numericality: { only_integer: true }
   validates :description, presence: true
-  validates :poster_image_url, presence: true
   validates :release_date, presence: true
   validate :release_date_is_in_future
+  validate :poster_image_file_or_url
+
+  mount_uploader :poster_image, ImageUploader
 
   def review_average
     if reviews.size > 0
@@ -21,6 +23,12 @@ class Movie < ActiveRecord::Base
   def release_date_is_in_future
     if release_date.present?
       errors.add(:release_date, "should be in the future") if release_date < Date.today
+    end
+  end
+
+  def poster_image_file_or_url
+    if !poster_image && !poster_image_url
+      errors.add(:poster_image, "file or url is required")
     end
   end
 
